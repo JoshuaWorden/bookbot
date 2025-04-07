77f599d043775a3569aee618dee5aba48899e613
Acebrick <josh.worden@live.com>
2025-04-07 20:14:45 +1000

    CH3-L1: Added sort_character_by_count()

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from stats import get_word_count
-from stats import get_character_count
+from stats import (get_word_count, 
+                   get_character_count, 
+                   sort_characters_by_count)
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    
+    # Get and print how many words are found in the text file
+    word_count = get_word_count(text)
+
+    # Get and print the number of times each character occurs in the text
+    character_count = get_character_count(text)
+    #print(character_count)
+
+    # Get and print the sorted character count that only includes letters
+    sorted_character_count = sort_characters_by_count(character_count)
+    print_character_count_report(sorted_character_count, word_count)
+
+
 
 # Return a string of the text passed in from the filepath
 def get_book_text(filepath):
@@ -8,17 +25,26 @@ def get_book_text(filepath):
         # Read the file and return it as a string
         # .read() returns a string
         return f.read()
-    
 
-def main():
-    text = get_book_text("books/frankenstein.txt")
-    
-    # Get and print how many words are found in the text file
-    word_count = get_word_count(text)
-    print(f"{word_count} words found in the document")
 
-    # Get and print the number of times each character occurs in the text
-    character_count = get_character_count(text)
-    print(character_count)
+
+def print_character_count_report(character_count, word_count):
+    # Print the program title and the book it's analysing
+    print("============ BOOKBOT ============\n" 
+    "Analyzing book found at books/frankenstein.txt...")
+    
+    # Print the word count of the book
+    print("----------- Word Count ----------\n" 
+    f"Found {word_count} total words")
+
+    # Print the character count of each letter
+    print("--------- Character Count -------")
+    for dict in character_count:       
+            print(f"{dict["character"]}: {dict["count"]}\n")
+    
+    # Print the footer
+    print("============= END ===============")
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -4,6 +4,9 @@ def get_word_count(text):
         list_of_words = text.split()
         return len(list_of_words)
 
+
+
+# Returns the number of times each character is found in the text
 def get_character_count(text):
         # Convert all uppercase characters to lowercase as they are counted as the same
         text = text.lower()
@@ -20,4 +23,28 @@ def get_character_count(text):
                         character_count[text[i]] = 1
 
         return character_count
-                        
+
+
+
+# Sort the character_count list from largest to smallest
+def sort_characters_by_count(character_count):
+        sorted_character_count = []
+        
+        # Iterate through each key in the dictionary
+        for key in character_count:
+                # Check if the name of the key is a letter
+                if key.isalpha() == True:
+                    # Add a two key-value pair dictionary to the sorted list, one for the letter and the other for the count
+                    sorted_character_count.append({"character": key, "count": character_count[key]})
+
+        # Sort the list of dictionaries from highest to lowest using the value assigned in the "count" key of each dictionary
+        sorted_character_count.sort(reverse=True, key=sort_dict_by_count)
+
+        return sorted_character_count
+
+
+
+# Tells the .sort() in sort_characters_by_count() what key to sort by in the dictionary
+def sort_dict_by_count(dict):
+        return dict["count"]
+
